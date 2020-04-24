e5175849818fd0cf926a92284c24b34170151be5
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-04-24 21:03:35 +0100

    Update sqlinjection.py

--- core/sqlinjection.py
+++ core/sqlinjection.py
@@ -29,47 +29,55 @@ def inject(link,pay):
 def response_time(url):
     user_agent=random.choice(regex.USR_AGENTS)
     headers = {'User-Agent': user_agent } 
-    r = requests.get(url,headers=headers)   
-    r_time = int(r.elapsed.total_seconds())
-    return r_time
+    try:
+        r = requests.get(url,headers=headers)   
+        r_time = int(r.elapsed.total_seconds())
+        return r_time
+    except:
+        pass
+    
 
 
 
 def error_base(url):
     state=False
-    for god,bad in regex.SQL_INJECTION_ERROR_BASE.items():
-        god_r=requests.get(inject(url,god)).text
-       
-        bad_r=requests.get(inject(url,bad)).text
-        if len(god_r) != len(bad_r):
-            state=True
-            if nano.reflection(nano.inject_param(url,'SRtbT5lOuEg')) != True:
-                print("\033[91mPossibly SQL injection vulnerability\033[00m  ")
-                print(inject(url,god)+' | Response lenth:'+str(len(god_r))+'\n'+inject(url,bad)+' | Response lenth:'+str(len(bad_r)))
-                break
-            else:
-                print('\033[33;1mWarning can be false positives\033[00m')
-                print("\033[91mPossibly SQL injection vulnerability\033[00m  ")
-                print(inject(url,god)+' | Response lenth:'+str(len(god_r))+'\n'+inject(url,bad)+' | Response lenth:'+str(len(bad_r)))
-                break
+    try:
+        for god,bad in regex.SQL_INJECTION_ERROR_BASE.items():
+            god_r=requests.get(inject(url,god)).text
+            bad_r=requests.get(inject(url,bad)).text
+            if len(god_r) != len(bad_r):
+                state=True
+                if nano.reflection(nano.inject_param(url,'SRtbT5lOuEg')) != True:
+                    print("\033[91mPossibly SQL injection vulnerability\033[00m  ")
+                    print(inject(url,god)+' | Response lenth:'+str(len(god_r))+'\n'+inject(url,bad)+' | Response lenth:'+str(len(bad_r)))
+                    break
+                else:
+                    print('\033[33;1mWarning can be false positives\033[00m')
+                    print("\033[91mPossibly SQL injection vulnerability\033[00m  ")
+                    print(inject(url,god)+' | Response lenth:'+str(len(god_r))+'\n'+inject(url,bad)+' | Response lenth:'+str(len(bad_r)))
+                    break
+    except:
+        pass
     return state
 
 
 def blind_base(url):
     state=False
-    for x in regex.SQL_INJECTION_BLIND_BASE:
-        
-        r1=inject(url,str(x).format('0'))
-        rs1=response_time(r1)
-        r2=inject(url,str(x).format('1'))
-        rs2=response_time(r2)
-        r3=inject(url,str(x).format('3'))
-        rs3=response_time(r3)
-        if int(rs1) < int(rs2) and int(rs2) < int(rs3):
-            state=True
-            print("\033[91mPossibly SQL injection  vulnerability\033[00m  ")
-            print(r1+' | Response time:'+str(rs1)+'\n'+r2+' | Response time:'+str(rs2)+'\n'+r3+' | Response time:'+str(rs3))
-            break
+    try:
+        for x in regex.SQL_INJECTION_BLIND_BASE:
+            r1=inject(url,str(x).format('0'))
+            rs1=response_time(r1)
+            r2=inject(url,str(x).format('1'))
+            rs2=response_time(r2)
+            r3=inject(url,str(x).format('3'))
+            rs3=response_time(r3)
+            if int(rs1) < int(rs2) and int(rs2) < int(rs3):
+                state=True
+                print("\033[91mPossibly SQL injection  vulnerability\033[00m  ")
+                print(r1+' | Response time:'+str(rs1)+'\n'+r2+' | Response time:'+str(rs2)+'\n'+r3+' | Response time:'+str(rs3))
+                break
+    except:
+        pass
     return state
   
 
@@ -81,14 +89,16 @@ def semple(url):
     try:
         r = requests.get(url,headers=headers,verify=False)
         cont = r.content
+        for x in regex.SQL_ERROR:
+            if(re.search(x, str(cont))):
+                state=True
+                print("\033[91mPossibly SQL injection vulnerability\033[00m  "+url)
+                break
     except:
         pass
     
-    for x in regex.SQL_ERROR:
-        if(re.search(x, str(cont))):
-            state=True
-            print("\033[91mPossibly SQL injection vulnerability\033[00m  "+url)
-            break
+    
+            
     return state
                 
                 
