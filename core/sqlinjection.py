4e5e9eb40c5a253c8640eb37dd86f7c8f94b5a4d
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-05-07 01:47:32 +0100

    Update sqlinjection.py

--- core/sqlinjection.py
+++ core/sqlinjection.py
@@ -52,12 +52,12 @@ def error_base(url):
                     state=True
                     if nano.reflection(nano.inject_param(url,'SRtbT5lOuEg')) != True:
                         print("\033[91mPossibly SQL injection vulnerability\033[00m  ")
-                        print(inject(url,god)+' | Content-Length:'+str(len(god_r))+'\n'+inject(url,bad)+' | Content-Length:'+str(len(bad_r)))
+                        print(inject(url,god)+' | Content-Length: '+str(god_r.headers.get('Content-Length'))+'\n'+inject(url,bad)+' | Content-Length: '+str(bad_r.headers.get('Content-Length')))
                         break
                     else:
                         print('\033[33;1mWarning can be false positives\033[00m')
                         print("\033[91mPossibly SQL injection vulnerability\033[00m  ")
-                        print(inject(url,god)+' | Response lenth: '+str(god_r.headers.get('Content-Length'))+'\n'+inject(url,bad)+' | Response lenth: '+str(bad_r.headers.get('Content-Length')))
+                        print(inject(url,god)+' | Content-Length: '+str(god_r.headers.get('Content-Length'))+'\n'+inject(url,bad)+' | Content-Length: '+str(bad_r.headers.get('Content-Length')))
                         break
     except:
         pass
