e8007e47d96c1b98cac88ed0e50a4b3cb402f0f8
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-05-09 20:36:08 +0100

    Update thorin.py

--- thorin.py
+++ thorin.py
@@ -18,11 +18,34 @@ import sys
 import os
 from time import sleep
 
-
-url=sys.argv[1]
 os.system('resize -s 25 120 > /dev/null')
-connter=[] 
-uniq=[]
+url=sys.argv[1]
+urls= archiveurl.waybackurls(url)
+js=[]
+pr=[]
+Dir=[]
+na=[]
+
+for i in urls:
+    for uniq_url in nano.inject_dir(i,"uNiq_stRiNg"):
+             if uniq_url not in Dir:
+                 Dir.append(uniq_url)
+    if '?' in i :
+        i_=nano.inject_param(i,"UniQ_sTrInG")
+        if i_ not in pr:
+            pr.append(i_)
+    elif nano.rev(i).split('.')[0] == nano.rev('js') or ".js?" in i:
+        if i not in js:
+            js.append(i)
+    else:
+        if i not in na:
+            na.append(i)
+
+
+
+
+
+
 def xssF(i):
     xss.xss_(i)
 
@@ -71,56 +94,48 @@ def lfi_dirF(i):
 def crlf_dirF(i):
     crlf.crlf_dir(i)
 
- 
-urls= archiveurl.waybackurls(url)
-with alive_bar(len(urls)) as bar:
-    for i in urls:
-        sleep(0.5)
+with alive_bar(len(na)) as bar:
+    for i in na:
         bar()
         i=i.rstrip()
         p1 = Process(target=traceF, args=(i,))
         p1.start()
-        p2 = Process(target=jsparseF, args=(i,))
-        p2.start()
         p4 = Process(target=base64F, args=(i,))
         p4.start()
-        p5 = Process(target=corsF, args=(i,))
-        p5.start()
-        
-        for uniq_url in nano.inject_dir(i,"uNiq_stRiNg"):
-             if uniq_url not in uniq:
-                 uniq.append(uniq_url)
-        if "?" in i:
-            uniq_link=nano.inject_param(i,'yaTi8CP7Efh')
-   
-        else:
-            uniq_link=i
-        
-        if uniq_link not in connter:
-            connter.append(uniq_link)
-            if '?' in i:
-                p6 = Process(target=xssF, args=(i,))
-                p6.start()
-                p7 = Process(target=open_redirectionF, args=(i,))
-                p7.start()
-                
-                p8 = Process(target=sqlscanF, args=(i,))
-                p8.start()
-                p9= Process(target=sstiscanF, args=(i,))
-                p9.start()
-                p10 = Process(target=lfiscanF, args=(i,))
-                p10.start()
-                p11 = Process(target=crlfscanF, args=(i,))
-                p11.start()
-                p12 = Process(target=oscommandF, args=(i,))
-                p12.start()
+with alive_bar(len(js)) as bar:
+    for i in js:
+        sleep(0.3)
+        bar()
+        i=i.rstrip()
+        p2 = Process(target=jsparseF, args=(i,))
+        p2.start()
+with alive_bar(len(pr)) as bar:
+    for i in pr:
+        sleep(0.5)
+        bar()       
+        p6 = Process(target=xssF, args=(i,))
+        p6.start()
+        p7 = Process(target=open_redirectionF, args=(i,))
+        p7.start()    
+        p8 = Process(target=sqlscanF, args=(i,))
+        p8.start()
+        p9= Process(target=sstiscanF, args=(i,))
+        p9.start()
+        p10 = Process(target=lfiscanF, args=(i,))
+        p10.start()
+        p11 = Process(target=crlfscanF, args=(i,))
+        p11.start()
+        p12 = Process(target=oscommandF, args=(i,))
+        p12.start()
                 
 
-with alive_bar(len(uniq)) as bar:
-    for i in uniq:
+with alive_bar(len(Dir)) as bar:
+    for i in Dir:
         sleep(2)
         i=i.rstrip()
         bar()
+        p5 = Process(target=corsF, args=(i,))
+        p5.start()
         p3 = Process(target=put_methodeF, args=(i,))
         p3.start()
         p14 = Process(target=redirection_dirF, args=(i,))
