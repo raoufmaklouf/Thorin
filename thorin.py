a1eadbc158b2ff3e6017c7f607b6ca8400206ac1
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-05-10 02:57:44 +0100

    Update thorin.py

--- thorin.py
+++ thorin.py
@@ -18,34 +18,11 @@ import sys
 import os
 from time import sleep
 
-os.system('resize -s 25 120 > /dev/null')
+
 url=sys.argv[1]
-urls= archiveurl.waybackurls(url)
-js=[]
-pr=[]
-Dir=[]
-na=[]
-
-for i in urls:
-    for uniq_url in nano.inject_dir(i,"uNiq_stRiNg"):
-             if uniq_url not in Dir:
-                 Dir.append(uniq_url)
-    if '?' in i :
-        i_=nano.inject_param(i,"UniQ_sTrInG")
-        if i_ not in pr:
-            pr.append(i_)
-    elif nano.rev(i).split('.')[0] == nano.rev('js') or ".js?" in i:
-        if i not in js:
-            js.append(i)
-    else:
-        if i not in na:
-            na.append(i)
-
-
-
-
-
-
+os.system('resize -s 25 120 > /dev/null')
+connter=[] 
+uniq=[]
 def xssF(i):
     xss.xss_(i)
 
@@ -94,43 +71,54 @@ def lfi_dirF(i):
 def crlf_dirF(i):
     crlf.crlf_dir(i)
 
-with alive_bar(len(na)) as bar:
-    for i in na:
+ 
+urls= archiveurl.waybackurls(url)
+with alive_bar(len(urls)) as bar:
+    for i in urls:
+        for uniq_url in nano.inject_dir(i,"uNiq_stRiNg"):
+             if uniq_url not in uniq:
+                 uniq.append(uniq_url)
+        if "?" in i:
+            uniq_link=nano.inject_param(i,'yaTi8CP7Efh')
+   
+        else:
+            uniq_link=i
+            
+        
         bar()
         i=i.rstrip()
         p1 = Process(target=traceF, args=(i,))
         p1.start()
-        p4 = Process(target=base64F, args=(i,))
-        p4.start()
-with alive_bar(len(js)) as bar:
-    for i in js:
-        sleep(0.3)
-        bar()
-        i=i.rstrip()
         p2 = Process(target=jsparseF, args=(i,))
         p2.start()
-with alive_bar(len(pr)) as bar:
-    for i in pr:
-        sleep(0.5)
-        bar()       
-        p6 = Process(target=xssF, args=(i,))
-        p6.start()
-        p7 = Process(target=open_redirectionF, args=(i,))
-        p7.start()    
-        p8 = Process(target=sqlscanF, args=(i,))
-        p8.start()
-        p9= Process(target=sstiscanF, args=(i,))
-        p9.start()
-        p10 = Process(target=lfiscanF, args=(i,))
-        p10.start()
-        p11 = Process(target=crlfscanF, args=(i,))
-        p11.start()
-        p12 = Process(target=oscommandF, args=(i,))
-        p12.start()
+        p4 = Process(target=base64F, args=(i,))
+        p4.start()
+        
+        
+        
+        
+        if uniq_link not in connter:
+            connter.append(uniq_link)
+            if '?' in i:
+                p6 = Process(target=xssF, args=(i,))
+                p6.start()
+                p7 = Process(target=open_redirectionF, args=(i,))
+                p7.start()
                 
+                p8 = Process(target=sqlscanF, args=(i,))
+                p8.start()
+                p9= Process(target=sstiscanF, args=(i,))
+                p9.start()
+                p10 = Process(target=lfiscanF, args=(i,))
+                p10.start()
+                p11 = Process(target=crlfscanF, args=(i,))
+                p11.start()
+                p12 = Process(target=oscommandF, args=(i,))
+                p12.start()
+                sleep(0.5)
 
-with alive_bar(len(Dir)) as bar:
-    for i in Dir:
+with alive_bar(len(uniq)) as bar:
+    for i in uniq:
         sleep(2)
         i=i.rstrip()
         bar()
@@ -146,4 +134,3 @@ with alive_bar(len(Dir)) as bar:
         p16.start()
         p17 = Process(target=crlf_dirF, args=(i,))
         p17.start()
-              
