8651bdfff9d62766b27a4dd34b789f2d54f09793
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-05-10 23:03:12 +0100

    Update cors.py

--- core/cors.py
+++ core/cors.py
@@ -15,14 +15,13 @@ def cors_(url):
         if "https://etdbeuajsyeu.com" in str(origen) and 'true' in str(credentials):
             print('\033[91m Possibly CORS vulnerability\033[00m  '+url)
         else:
-            pass
-        r=requests.post(url, headers=headers ,verify=False)
-        origen=r.headers.get('Access-Control-Allow-Origin')
-        credentials=r.headers.get('Access-Control-Allow-Credentials')
-        if "https://etdbeuajsyeu.com" in str(origen) and 'true' in str(credentials):
-            print('\033[91m Possibly CORS vulnerability\033[00m  '+url)
-        else:
-            pass
+            r=requests.post(url, headers=headers ,verify=False)
+            origen=r.headers.get('Access-Control-Allow-Origin')
+            credentials=r.headers.get('Access-Control-Allow-Credentials')
+            if "https://etdbeuajsyeu.com" in str(origen) and 'true' in str(credentials):
+                print('\033[91m Possibly CORS vulnerability\033[00m  '+url)
+            else:
+                pass
     except:
         pass
 
