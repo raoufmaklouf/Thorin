50bb840b0743019f3f32bc925ddc4dc98bba5c2c
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-05-21 01:31:18 +0100

    Update regex.py

--- core/regex.py
+++ core/regex.py
@@ -182,7 +182,7 @@ REGEX_JS = {
 "AMAZON_URL_2":"[a-z0-9.-].s3-[a-z0-9.-][.-](eu|ap|us|ca|sa|cn)",
 "AMAZON_URL_3":"s3.amazonaws.com/[a-z0-9._-]",
 "AMAZON_URL_4":"s3-[a-z0-9-].amazonaws.com/[a-z0-9._-]",
-"URL":"https?://[^\"\\'> ]+",
+#"URL":"https?://[^\"\\'> ]+",
 "AMAZON_KEY":"([^A-Z0-9]|^)(AKIA|A3T|AGPA|AIDA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Z0-9]{12,}",
 "Authorization":"^Bearer\s[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$",
 "accessToken":"^acesstoken=[0-9]{13,17}",
