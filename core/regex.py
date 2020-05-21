fb0ea964c8e8f79a906993c6fa6e0afabd3f2bad
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-05-21 01:14:26 +0100

    Update regex.py

--- core/regex.py
+++ core/regex.py
@@ -378,8 +378,10 @@ Base_64_white_list=[
 'ants',
 'fico',
 'bios',
- 'film',
- 'city'
+'LENS',
+'TWRP',
+'UTF8',
+'Kiwi'
 ]
 USR_AGENTS=[
 'Mozilla/5.0 (SymbianOS/9.2; U; Series60/3.1 NokiaE51-1/220.34.37; Profile/MIDP-2.0 Configuration/CLDC-1.1) AppleWebKit/413 (KHTML, like Gecko) Safari/413', 
