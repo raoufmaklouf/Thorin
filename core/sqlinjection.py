c2d9c650462307b77b584831caa7c08b677827a1
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-05-22 01:17:50 +0100

    Update sqlinjection.py

--- core/sqlinjection.py
+++ core/sqlinjection.py
@@ -7,24 +7,6 @@ from core import regex
 from requests.packages import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def inject(link,pay):
-    b_link=link.split('?')[0]
-    params=link.split('?')[1]
-    if len(params) >0:
-        param_list=[]
-        Plenth=params.split('&')
-        for z in Plenth:
-            param=z.split('=')[0]
-            val=z.split('=')[1]
-            if param not in param_list:
-                param_list.append(param)
-            payload=(b_link+'?')
-            for y in param_list:
-                payload=(payload+y+'='+val+pay+'&')
-            payload=payload[:-1]
-        return payload
-
-
 
 def response_time(url):
     user_agent=random.choice(regex.USR_AGENTS)
@@ -42,20 +24,20 @@ def response_time(url):
 def bolian_base(url):
     state=False
     try:
-        r1=requests.get(inject(url,'test')).text
-        r2=requests.get(inject(url,'test')).text
+        r1=requests.get(nano.inject(url,'test')).text
+        r2=requests.get(nano.inject(url,'test')).text
         if len(r1)==len(r2) :
             for god,bad in regex.SQL_INJECTION_ERROR_BASE.items():
-                r1=requests.get(inject(url,god))
+                r1=requests.get(nano.inject(url,god))
                 god_r=r1.content
                 sr1=r1.status_code
-                r2=requests.get(inject(url,bad))
+                r2=requests.get(nano.inject(url,bad))
                 bad_r=r2.content
                 sr2=r2.status_code
                 if len(god_r) != len(bad_r) and sr1 == sr2:
                     state=True
                     print("\033[91mPossibly SQL injection vulnerability\033[00m  ")
-                    print(inject(url,god)+' | response Length:'+str(len(god_r))+'\n'+inject(url,bad)+' | response Length:'+str(len(bad_r)))
+                    print(nano.inject(url,god)+' | response Length:'+str(len(god_r))+'\n'+nano.inject(url,bad)+' | response Length:'+str(len(bad_r)))
                     break
                     
     except:
@@ -67,11 +49,11 @@ def time_base(url):
         
     try:
         for x in regex.SQL_INJECTION_BLIND_BASE:
-            r1=inject(url,str(x).format('3'))
+            r1=nano.inject(url,str(x).format('3'))
             rs1=response_time(r1)
-            r2=inject(url,str(x).format('6'))
+            r2=nano.inject(url,str(x).format('6'))
             rs2=response_time(r2)
-            r3=inject(url,str(x).format('9'))
+            r3=nano.inject(url,str(x).format('9'))
             rs3=response_time(r3)
             if int(rs1) >= 3 and int(rs2) >= 6 and int(rs3) >= 9:
                 if int(rs1) >= int(rs2)/2 and int(rs1) >= int(rs3)/3:
@@ -100,21 +82,22 @@ def semple(url):
         if done ==1 :
             break
         try:
-            url=inject(url,i)
-            r = requests.get(url,headers=headers,verify=False)
+            url1=nano.inject(url,i)
+            r = requests.get(url1,headers=headers,verify=False)
             cont = r.content
             for x in regex.SQL_ERROR:
                 if(re.search(x, str(cont))):
-                    url_=nano.inject_param(url,"x")
-                    r_ = requests.get(url_,headers=headers,verify=False)
+                    r_ = requests.get(url,headers=headers,verify=False)
                     cont_ = r_.content
-                    if(re.search(x, str(cont_))):
-                        break
-                    else:
+                    if not (re.search(x, str(cont_))):
                         state=True
-                        print("\033[91mPossibly SQL injection vulnerability\033[00m  "+url)
+                        print("\033[91mPossibly SQL injection vulnerability\033[00m  "+url1)
                         done=1
                         break
+                        
+                    else:
+                        pass
+                        
         except:
             pass
                
