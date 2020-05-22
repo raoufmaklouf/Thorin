1c786fd31cafece11accb36b206496ebf9e2b333
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-05-22 01:44:07 +0100

    Update regex.py

--- core/regex.py
+++ core/regex.py
@@ -383,6 +383,7 @@ Base_64_white_list=[
 'UTF8',
 'Kiwi',
 'city',
+'ansh',
 ]
 USR_AGENTS=[
 'Mozilla/5.0 (SymbianOS/9.2; U; Series60/3.1 NokiaE51-1/220.34.37; Profile/MIDP-2.0 Configuration/CLDC-1.1) AppleWebKit/413 (KHTML, like Gecko) Safari/413', 
