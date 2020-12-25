c8b635e32e08ca953c0f454d136db0c8ddb5105c
raoufmaklouf <raoufmaklouf0int@gmail.com>
2020-12-25 22:27:28 +0100

    Add files via upload

---
+++ core/ssrf.py
@@ -0,0 +1,27 @@
+import re 
+import requests
+import random
+from core import regex
+from core import nano
+from core import bot
+from core import config
+from requests.packages import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) 
+
+
+def ssrf_(i):
+    
+    try:
+        url=nano.inject_param(i,config.ssrf_link)
+        user_agent=random.choice(regex.USR_AGENTS)
+        headers = {'User-Agent': user_agent } 
+        r = requests.get(url ,headers=headers ,verify=False )       
+        resp = r.content
+        x = re.findall(config.ssrf_regix, str(resp))
+        if (x):
+            print('\033[91mPossibly SSRF vulnerability\033[00m  '+url)
+            msg="Possibly SSRF vulnerability "+url
+            bot.telegram_bot_sendtext(msg)
+    except:
+        pass
+                       
