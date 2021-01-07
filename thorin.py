fcbd8500a546f361c75443896218c4300a8e6d02
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-01-07 15:47:22 +0000

    Update thorin.py

--- thorin.py
+++ thorin.py
@@ -7,6 +7,7 @@ from core import lfi
 from core import crlf
 from core import nano
 from core import trace
+from core import jsparse
 from core import oscommand
 from core import cors
 from core import ssrf
@@ -89,13 +90,13 @@ with alive_bar(len(urls)) as bar:
     for i in urls:
         bar()
         i=i.rstrip()
-        p1 = Process(target=traceF, args=(i,))
+        p1 = Process(target=traceF, args=(i,),daemon=True)
         p1.start()
         
-        p2 = Process(target=credentialsFondF, args=(i,))
+        p2 = Process(target=credentialsFondF, args=(i,),daemon=True)
         p2.start()      
         
-        p4 = Process(target=base64F, args=(i,))
+        p4 = Process(target=base64F, args=(i,),daemon=True)
         p4.start()
 
         p2.join()
@@ -110,7 +111,7 @@ with alive_bar(len(urls)) as bar:
                 if dlink not in uniqlink :
                     uniqlink.append(dlink)
                     url=dlink
-                    px = Process(target=dirvulscanF, args=(url,))
+                    px = Process(target=dirvulscanF, args=(url,),daemon=True)
                     px.start()                    
                     px.join()
                 else:
@@ -121,28 +122,28 @@ with alive_bar(len(urls)) as bar:
             if plink not in paramlink:
                  paramlink.append(plink)
                  url=plink
-                 p6 = Process(target=xssF, args=(url,))
+                 p6 = Process(target=xssF, args=(url,),daemon=True)
                  p6.start()                 
                  
-                 p7 = Process(target=open_redirectionF, args=(url,))
+                 p7 = Process(target=open_redirectionF, args=(url,),daemon=True)
                  p7.start() 
                  
-                 p8 = Process(target=sqlscanF, args=(url,))
+                 p8 = Process(target=sqlscanF, args=(url,),daemon=True)
                  p8.start()
                  
-                 p9= Process(target=sstiscanF, args=(url,))
+                 p9= Process(target=sstiscanF, args=(url,),daemon=True)
                  p9.start()                 
                  
-                 p10 = Process(target=lfiscanF, args=(url,))
+                 p10 = Process(target=lfiscanF, args=(url,),daemon=True)
                  p10.start()                 
                  
-                 p11 = Process(target=crlfscanF, args=(url,))
+                 p11 = Process(target=crlfscanF, args=(url,),daemon=True)
                  p11.start()                 
                  
-                 p12 = Process(target=oscommandF, args=(url,))
+                 p12 = Process(target=oscommandF, args=(url,),daemon=True)
                  p12.start()                 
                  
-                 p13 = Process(target=ssrfF, args=(url,))
+                 p13 = Process(target=ssrfF, args=(url,),daemon=True)
                  p13.start()                 
                  
                  p13.join()
@@ -160,7 +161,7 @@ with alive_bar(len(urls)) as bar:
                 if dlink not in uniqlink :
                     uniqlink.append(dlink)
                     url=dlink
-                    px = Process(target=dirvulscanF, args=(url,))
+                    px = Process(target=dirvulscanF, args=(url,),daemon=True)
                     px.start()                    
                     px.join()
                 else:
