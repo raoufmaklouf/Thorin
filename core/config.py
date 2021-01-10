201ff928029c24ab38e3662e9f82022480b72508
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-01-10 11:11:56 +0100

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,4 +1,4 @@
 ssrf_regix='k6unx4pudf8k5itoapaxjwzjigz'
-ssrf_link='http://omeg0ivn7k95wloyezah7zcqghm7aw.burpcollaborator.net/'
+ssrf_link='http://omeg0ivn7k95wloyezah7zcqghm7aw.burpcollaborator.net'
 bot_token = '0123456789:AAGRfCBUHOkgZ0ymJ7Jkx_PYGeJ2tbYzyqU'
 bot_chatID = '0123456789'
