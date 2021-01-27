39e0dac94faf93eb25457e5368a4643f8f9c9e24
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-01-27 21:24:33 +0000

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -1,14 +1,16 @@
 import requests
+from core import config
+
 
 def telegram_bot_sendtext(bot_message):
     
-    bot_token = ''
-    bot_chatID = ''
+    bot_token = config.bot_token
+    bot_chatID = config.bot_chatID
     send_text = 'https://api.telegram.org/bot'+ bot_token+'/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
-    response = requests.get(send_text)
+    send = requests.get(send_text)
 
-    return response.json()
+   
     
 
 
