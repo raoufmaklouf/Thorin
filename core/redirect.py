95c71aadbd3321d2b97e63969ef49b54401feba1
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-01-30 13:30:42 +0000

    Update redirect.py

--- core/redirect.py
+++ core/redirect.py
@@ -23,57 +23,8 @@ def build_wordlist():
     return words
 
 def run(word_queue,url):
-    url=str(url)
-    for x in regex.parameters_OR:
-            param=(x+"=")
-            if url != None and  param in url:
-                try:
-                    base_link=url.split(param)[0]
-                except:
-                    pass
-                f_link=base_link+param
-                while not word_queue.empty():
-                    attempt = word_queue.get()
-                    attempt_list = []
-                    attempt_list.append(attempt)
-        
-                    for brute in attempt_list:
-                        user_agent=random.choice(regex.USR_AGENTS)
-                        headers = {'User-Agent': user_agent } 
-                        url = f_link+brute
-                        
-                       
-                        try:         
-                            r = requests.get(url, headers=headers,verify=False)
-                            resp=r.content       
-                            if  "Location" in r.headers and "google" in r.headers :
-                                print('\033[91m Possibly open redirection vulnerability\033[00m\t'+url)
-                                break
-                            else:
-                                pass
-                        except :
-                            pass
-           
-word_queue = build_wordlist()
-def redirect_(url):
-    for i in range(threads):
-        t = threading.Thread(target=run,args=(word_queue,url))
-        t.start()
-        
-
-threads_dir =20
-
-def build_wordlist_dir():
-  
-    words= queue.Queue()
-    for word in regex.payloads_OR_d:
-        word = word.rstrip()
-        words.put(word)
-        
-    return words
-
-def run_dir(word_queue,url):
-     
+    url=nano.inject_param(url,regex.payloads_OR_p)
+    
     while not word_queue.empty():
         attempt = word_queue.get()
         attempt_list = []
@@ -82,21 +33,25 @@ def run_dir(word_queue,url):
         for brute in attempt_list:
             user_agent=random.choice(regex.USR_AGENTS)
             headers = {'User-Agent': user_agent } 
-            url=str(url).replace('uNiq_stRiNg',str(brute))
+            url = url=nano.inject_param(url,str(brute))
+                        
+                       
             try:
                 r = requests.get(url,headers=headers,verify=False)
-                resp=r.content       
-                if "Location" in r.headers and "google" in r.headers :
-                    print('\033[91m Possibly open redirection vulnerability\033[00m  '+url)
-                    break
-                else:
-                    pass
+                h=r.history
+       
+                for red in h:
+                    nexturl=red.url
+                    x = re.findall('google', str(nexturl))
+                    if (x):
+                        print('\033[91m Possibly open redirection vulnerability\033[00m '+url)                                
+                                          
+                            
             except :
                 pass
            
-word_queue_dir = build_wordlist_dir()
-def redirect_dir(url):
-    for i in range(threads_dir):
-        t = threading.Thread(target=run_dir,args=(word_queue_dir,url))
+word_queue = build_wordlist()
+def redirect_(url):
+    for i in range(threads):
+        t = threading.Thread(target=run,args=(word_queue,url))
         t.start()
-       
