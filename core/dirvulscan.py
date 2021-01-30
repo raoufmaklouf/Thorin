00eba0a13cc4daef619430b0cfdfd3b9e6a117fa
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-01-30 13:31:52 +0000

    Update dirvulscan.py

--- core/dirvulscan.py
+++ core/dirvulscan.py
@@ -6,46 +6,24 @@ import threading
 import queue
 from multiprocessing import Process
 from core import bot
+from core import regex
 
 from requests.packages import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  
-threads =30
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-USR_AGENTS=[
-'Mozilla/5.0 (SymbianOS/9.2; U; Series60/3.1 NokiaE51-1/220.34.37; Profile/MIDP-2.0 Configuration/CLDC-1.1) AppleWebKit/413 (KHTML, like Gecko) Safari/413', 
-'Mozilla/5.0 (SymbianOS/9.2; U; Series60/3.1 NokiaN95/11.0.026; Profile MIDP-2.0 Configuration/CLDC-1.1) AppleWebKit/413 (KHTML, like Gecko) Safari/413', 
-'Mozilla/5.0 (SymbianOS/9.3; U; Series60/3.2 Nokia5630d-1/012.020; Profile MIDP-2.1 Configuration/CLDC-1.1) AppleWebKit/413 (KHTML, like Gecko) Safari/413', 
-'Mozilla/5.0 (SymbianOS/9.3; U; Series60/3.2 NokiaN78-1/12.046; Profile/MIDP-2.1 Configuration/CLDC-1.1) AppleWebKit/413 (KHTML, like Gecko) Safari/413', 
-'Mozilla/5.0 (SymbianOS/9.4; Series60/5.0 NokiaN97-3/21.2.045; Profile/MIDP-2.1 Configuration/CLDC-1.1;) AppleWebKit/525 (KHTML, like Gecko) BrowserNG/7.1.4', 
-'Mozilla/5.0 (SymbianOS/9.4; U; Series60/5.0 Nokia5530c-2/10.0.050; Profile MIDP-2.1 Configuration/CLDC-1.1) AppleWebKit/525 (KHTML, like Gecko) Safari/525', 
-'Mozilla/5.0 (SymbianOS/9.4; U; Series60/5.0 Nokia5800d-1/31.0.101; Profile MIDP-2.1 Configuration/CLDC-1.1) AppleWebKit/413 (KHTML, like Gecko) Safari/413', 
-'Mozilla/5.0 (webOS/1.4.0; U; en-US) AppleWebKit/532.2 (KHTML, like Gecko) Version/1.0 Safari/532.2 Pre/1.0', 
-'Mozilla/5.0 (webOS/Palm webOS 1.2.9; U; en-US) AppleWebKit/525.27.1 (KHTML, like Gecko) Version/1.0 Safari/525.27.1 Pixi/1.0', 
-'Mozilla/5.0 (Windows; U; Win98; en-US; rv:0.9.2) Gecko/20010726 Netscape6/6.1', 
-'Mozilla/5.0 (Windows; U; Win98; en-US; rv:x.xx) Gecko/20030423 Firebird Browser/0.6', 
-'Mozilla/5.0 (Windows; U; Win9x; en; Stable) Gecko/20020911 Beonex/0.8.1-stable', 
-'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19', 
-'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.5) Gecko/20060731 Firefox/1.5.0.5 Flock/0.7.4.1', 
-'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.1) Gecko/2008092215 Firefox/3.0.1 Orca/1.1 beta 3', 
-'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.x.x) Gecko/20041107 Firefox/x.x', 
-'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.xx) Gecko/20030504 Mozilla Firebird/0.6', 
-'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.xxx) Gecko/20041027 Mnenhy/0.6.0.104', 
-'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9b5) Gecko/2008032620 Firefox/3.0b5', 
-'Mozilla/5.0 (Windows; U;XMPP Tiscali Communicator v.10.0.1; Windows NT 5.1; it; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3', 
-'Mozilla/5.0 (X11; Linux i686; U;rv: 1.7.13) Gecko/20070322 Kazehakase/0.4.4.1', 
-'Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01', 
-'Mozilla/5.0 (X11; U; Linux armv7l; en-GB; rv:1.9.2b6pre) Gecko/20100318 Firefox/3.5 Maemo Browser 1.7.4.8 RX-51 N900', 
-'Mozilla/5.0 (X11; U; Linux i686; de-AT; rv:1.8.0.2) Gecko/20060309 SeaMonkey/1.0', 
-'Mozilla/5.0 (X11; U; Linux i686; en-GB; rv:1.7.6) Gecko/20050405 Epiphany/1.6.1 (Ubuntu) (Ubuntu package 1.0.2)', 
-'Mozilla/5.0 (X11; U; Linux i686; en-US; Nautilus/1.0Final) Gecko/20020408', 
-'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:0.9.3) Gecko/20010801', 
-'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.2b) Gecko/20021007 Phoenix/0.3', 
-'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.6) Gecko/20040413 Epiphany/1.2.1', 
-'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.7) Gecko/20060909 Firefox/1.5.0.7 SnapPreviewBot', 
-'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1) Gecko/20061129 BonEcho/2.0', 
-'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.1) Gecko/20061205 Iceweasel/2.0.0.1 (Debian-2.0.0.1+dfsg-2)', 
-'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9a8) Gecko/2007100619 GranParadiso/3.0a8', 
-]
+threads =30
+USR_AGENTS=regex.USR_AGENTS
+
+def rev(str_):
+        linth=len(str_)
+        rs=''
+        for x in range(linth+1):
+            if x ==0:
+                pass
+            else:
+                b=str_[-x]
+                rs+=b
+        return rs
 
 def build_wordlist(urls):
     words= queue.Queue()
@@ -156,20 +134,26 @@ def openredaraction_(i):
     "?Page=ZXZpbC5jb20=&_url=ZXZpbC5jb20=&callback=ZXZpbC5jb20=&checkout_url=ZXZpbC5jb20=&content=ZXZpbC5jb20=&continue=ZXZpbC5jb20=&continueTo=ZXZpbC5jb20=&counturl=ZXZpbC5jb20=&data=ZXZpbC5jb20=&dest=ZXZpbC5jb20=&dest_url=ZXZpbC5jb20=&dir=ZXZpbC5jb20=&document=ZXZpbC5jb20=&domain=ZXZpbC5jb20=&done=ZXZpbC5jb20=&download=ZXZpbC5jb20=&feed=ZXZpbC5jb20=&file=ZXZpbC5jb20=&host=ZXZpbC5jb20=&html=ZXZpbC5jb20=&http=ZXZpbC5jb20=&https=ZXZpbC5jb20=&image=ZXZpbC5jb20=&image_src=ZXZpbC5jb20=&image_url=ZXZpbC5jb20=&imageurl=ZXZpbC5jb20=&include=ZXZpbC5jb20=&langTo=ZXZpbC5jb20=&media=ZXZpbC5jb20=&navigation=ZXZpbC5jb20=&next=ZXZpbC5jb20=&open=ZXZpbC5jb20=&out=ZXZpbC5jb20=&page=ZXZpbC5jb20=&page_url=ZXZpbC5jb20=&pageurl=ZXZpbC5jb20=&path=ZXZpbC5jb20=&picture=ZXZpbC5jb20=&port=ZXZpbC5jb20=&proxy=ZXZpbC5jb20=&redir=ZXZpbC5jb20=&redirect=ZXZpbC5jb20=&redirectUri=ZXZpbC5jb20=&redirectUrl=ZXZpbC5jb20=&reference=ZXZpbC5jb20=&referrer=ZXZpbC5jb20=&req=ZXZpbC5jb20=&request=ZXZpbC5jb20=&retUrl=ZXZpbC5jb20=&return=ZXZpbC5jb20=&returnTo=ZXZpbC5jb20=&return_path=ZXZpbC5jb20=&return_to=ZXZpbC5jb20=&rurl=ZXZpbC5jb20=&show=ZXZpbC5jb20=&site=ZXZpbC5jb20=&source=ZXZpbC5jb20=&src=ZXZpbC5jb20=&target=ZXZpbC5jb20=&to=ZXZpbC5jb20=&uri=ZXZpbC5jb20=&url=ZXZpbC5jb20=&val=ZXZpbC5jb20=&validate=ZXZpbC5jb20=&view=ZXZpbC5jb20=&window=ZXZpbC5jb20=&redirect_to=ZXZpbC5jb20=",
 "?Page=aHR0cDovL2V2aWwuY29t&_url=aHR0cDovL2V2aWwuY29t&callback=aHR0cDovL2V2aWwuY29t&checkout_url=aHR0cDovL2V2aWwuY29t&content=aHR0cDovL2V2aWwuY29t&continue=aHR0cDovL2V2aWwuY29t&continueTo=aHR0cDovL2V2aWwuY29t&counturl=aHR0cDovL2V2aWwuY29t&data=aHR0cDovL2V2aWwuY29t&dest=aHR0cDovL2V2aWwuY29t&dest_url=aHR0cDovL2V2aWwuY29t&dir=aHR0cDovL2V2aWwuY29t&document=aHR0cDovL2V2aWwuY29t&domain=aHR0cDovL2V2aWwuY29t&done=aHR0cDovL2V2aWwuY29t&download=aHR0cDovL2V2aWwuY29t&feed=aHR0cDovL2V2aWwuY29t&file=aHR0cDovL2V2aWwuY29t&host=aHR0cDovL2V2aWwuY29t&html=aHR0cDovL2V2aWwuY29t&http=aHR0cDovL2V2aWwuY29t&https=aHR0cDovL2V2aWwuY29t&image=aHR0cDovL2V2aWwuY29t&image_src=aHR0cDovL2V2aWwuY29t&image_url=aHR0cDovL2V2aWwuY29t&imageurl=aHR0cDovL2V2aWwuY29t&include=aHR0cDovL2V2aWwuY29t&langTo=aHR0cDovL2V2aWwuY29t&media=aHR0cDovL2V2aWwuY29t&navigation=aHR0cDovL2V2aWwuY29t&next=aHR0cDovL2V2aWwuY29t&open=aHR0cDovL2V2aWwuY29t&out=aHR0cDovL2V2aWwuY29t&page=aHR0cDovL2V2aWwuY29t&page_url=aHR0cDovL2V2aWwuY29t&pageurl=aHR0cDovL2V2aWwuY29t&path=aHR0cDovL2V2aWwuY29t&picture=aHR0cDovL2V2aWwuY29t&port=aHR0cDovL2V2aWwuY29t&proxy=aHR0cDovL2V2aWwuY29t&redir=aHR0cDovL2V2aWwuY29t&redirect=aHR0cDovL2V2aWwuY29t&redirectUri=aHR0cDovL2V2aWwuY29t&redirectUrl=aHR0cDovL2V2aWwuY29t&reference=aHR0cDovL2V2aWwuY29t&referrer=aHR0cDovL2V2aWwuY29t&req=aHR0cDovL2V2aWwuY29t&request=aHR0cDovL2V2aWwuY29t&retUrl=aHR0cDovL2V2aWwuY29t&return=aHR0cDovL2V2aWwuY29t&returnTo=aHR0cDovL2V2aWwuY29t&return_path=aHR0cDovL2V2aWwuY29t&return_to=aHR0cDovL2V2aWwuY29t&rurl=aHR0cDovL2V2aWwuY29t&show=aHR0cDovL2V2aWwuY29t&site=aHR0cDovL2V2aWwuY29t&source=aHR0cDovL2V2aWwuY29t&src=aHR0cDovL2V2aWwuY29t&target=aHR0cDovL2V2aWwuY29t&to=aHR0cDovL2V2aWwuY29t&uri=aHR0cDovL2V2aWwuY29t&url=aHR0cDovL2V2aWwuY29t&val=aHR0cDovL2V2aWwuY29t&validate=aHR0cDovL2V2aWwuY29t&view=aHR0cDovL2V2aWwuY29t&window=aHR0cDovL2V2aWwuY29t&redirect_to=aHR0cDovL2V2aWwuY29t"
     ]
-    regix='(?m)^(?:Location\s*?:\s*?)(?:https?://|//)?(?:[a-zA-Z0-9\-_]*\.)?evil\.com(?:\s*?)$'
+    
     for x in lis:
         x=x.rstrip()
         url=i+'/'+x
         try:
-            r = requests.head(url,verify=False)
-            resp = r.content
-            x = re.findall(regix, str(resp))
-            if (x):
-                print('\033[91mPossibly open redrection vulnerability\033[00m  '+url)
-                msg="Possibly open redrection vulnerability "+url
-                bot.telegram_bot_sendtext(msg)
-        except:
-            pass
+            r = requests.get(url,headers=headers,verify=False)
+            h=r.history
+       
+            for red in h:
+                nexturl=red.url
+                x = re.findall('evil', str(nexturl))
+                if (x):
+                    print('\033[91m Possibly open redirection vulnerability\033[00m '+url) 
+                    msg="Possibly open redrection vulnerability "+url
+                    bot.telegram_bot_sendtext(msg)                               
+                                          
+                            
+            except :
+                pass
+        
 
 def lfi_(i):
     ul=i
@@ -251,30 +235,12 @@ def lfi_(i):
         
 
 def backupfile_(i):
-    
-    
-    try:
-       i=i.replace(':80','')
-    except:
-       pass
+
     filename=[]
     ext=['.tar','.rar','.zip','.tmp','.tar.gz','.sql.gz','.bak.sql','.bak.sql.gz', '.bak.sql.bz2','.bak.sql.tar.gz','.txt','.bak','.bak1','.bakup','.bakup1',  '.bkp','.save','.old','.orig','.original','.sql','.tpl','.tmp','.temp','.saved','.back','.bck','.bakup','.nsx','.cs','.csproj',
     '.vb','.0','.1','.2','.arc','.inc','.lst',]
     urls=[]
-    def rev(str_):
-        linth=len(str_)
-        rs=''
-        for x in range(linth+1):
-            if x ==0:
-                pass
-            else:
-                b=str_[-x]
-                rs+=b
-        return rs
-        
     
-         
-
     subdomainmame=i.split('/')[2]
     filename.append(subdomainmame)
     subdomnameMod1=subdomainmame.replace('.','_')
@@ -319,7 +285,7 @@ def backupfile_(i):
             attempt_list = []
             attempt_list.append(attempt)
             for brute in attempt_list:
-                #print(brute) 
+                
                 try:
                      r = requests.head(brute,verify=False)
                      scode=r.status_code
@@ -352,11 +318,3 @@ def run(i):
     p5.start()
     p6 = Process(target=backupfile_, args=(i,))
     p6.start()
-   
-   
-    
-    
-  
-  
-        
-
