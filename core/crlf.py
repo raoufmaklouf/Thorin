ede884a84acda3808549ff6c2375ce21ead2728a
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-02-02 13:30:21 +0000

    Update crlf.py

--- core/crlf.py
+++ core/crlf.py
@@ -48,31 +48,3 @@ def crlf_(url):
         t = threading.Thread(target=run_,args=(word_queue,url))
         t.start()
         
-
-
-
-def run_dir(word_queue,url):
-    while not word_queue.empty():
-        attempt = word_queue.get()
-        attempt_list = []
-        attempt_list.append(attempt)
-        
-        for brute in attempt_list:
-            try:
-                session = requests.Session()
-                furl=str(url).replace('uNik_stRiNg',brute)
-                session.get(furl)
-                if 'crlf' in session.cookies.get_dict() and 'injection' in session.cookies.get_dict().values():
-                    print('\033[91m Possibly CRLF injection vulnerability\033[00m  '+furl)
-                    break
-                else :
-                    pass
-            except :
-                pass
-    
-
-def crlf_dir(url):
-    for i in range(threads):
-        t = threading.Thread(target=run_dir,args=(word_queue,url))
-        t.start()
-        
