4c71d8bb1c33ee05d2b3f6aa4bf2da3cee4d4508
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-02-02 14:44:27 +0000

    Update thorin.py

--- thorin.py
+++ thorin.py
@@ -13,6 +13,7 @@ from core import ssrf
 from core import base_64
 from core import credentialsFond
 from core import dirvulscan
+from core import regex
 from multiprocessing import Process
 from alive_progress import alive_bar
 import sys
@@ -109,87 +110,96 @@ def credentialsFondF(i):
     credentialsFond.CredentialsFond_(i)
 
  
-
+static=[]
 with alive_bar(len(urls)) as bar:
     for i in urls:
         bar()
         i=i.rstrip()
-        p1 = Process(target=traceF, args=(i,))
-        p1.start()
-        
-        p2 = Process(target=credentialsFondF, args=(i,))
-        p2.start()      
-        
-        p4 = Process(target=base64F, args=(i,))
-        p4.start()
+        if '?' in i:
+            chekUrl=i.split['?'][1]
+        else:
+            chekUrl=i
+        for x in regex.STATIC_EXT:
+            if chekUrl.endswith(x):
+                static.append(chekUrl)
 
-        p2.join(timeout=5)
-        p1.join(timeout=5)
-        p4.join(timeout=5)
+        if chekUrl not in static:
+            p1 = Process(target=traceF, args=(i,))
+            p1.start()
+        
+            p2 = Process(target=credentialsFondF, args=(i,))
+            p2.start()      
+        
+            p4 = Process(target=base64F, args=(i,))
+            p4.start()
+
+            p2.join(timeout=5)
+            p1.join(timeout=5)
+            p4.join(timeout=5)
         
 
 
-        if "?" in i:
-            path=i.split('?')[0]
-            for dlink in nano.inject_dir(path):
-                if dlink not in uniqlink :
-                    uniqlink.append(dlink)
-                    url=dlink
-                    px = Process(target=dirvulscanF, args=(url,))
-                    px.start()                    
-                    px.join(timeout=10)
-                else:
-                    pass
+            if "?" in i:
+                path=i.split('?')[0]
+                for dlink in nano.inject_dir(path):
+                    if dlink not in uniqlink :
+                        uniqlink.append(dlink)
+                        url=dlink
+                        px = Process(target=dirvulscanF, args=(url,))
+                        px.start()                    
+                        px.join(timeout=10)
+                    else:
+                        pass
                    
 
-            plink=nano.inject_param(i,'yaTi8CP7Efh')
-            if plink not in paramlink:
-                 paramlink.append(plink)
-                 url=plink
-                 p6 = Process(target=xssF, args=(url,))
-                 p6.start()                 
+                plink=nano.inject_param(i,'yaTi8CP7Efh')
+                if plink not in paramlink:
+                    paramlink.append(plink)
+                    url=plink
+                    p6 = Process(target=xssF, args=(url,))
+                    p6.start()                 
                  
-                 #p7 = Process(target=open_redirectionF, args=(url,))
-                 #p7.start() 
+                    #p7 = Process(target=open_redirectionF, args=(url,))
+                    #p7.start() 
                  
-                 p8 = Process(target=sqlscanF, args=(url,))
-                 p8.start()
+                    p8 = Process(target=sqlscanF, args=(url,))
+                    p8.start()
                  
-                 p9= Process(target=sstiscanF, args=(url,))
-                 p9.start()                 
+                    p9= Process(target=sstiscanF, args=(url,))
+                    p9.start()                 
                  
-                 p10 = Process(target=lfiscanF, args=(url,))
-                 p10.start()                 
+                    p10 = Process(target=lfiscanF, args=(url,))
+                    p10.start()                 
                  
-                 p11 = Process(target=crlfscanF, args=(url,))
-                 p11.start()                 
+                    p11 = Process(target=crlfscanF, args=(url,))
+                    p11.start()                 
                  
-                 p12 = Process(target=oscommandF, args=(url,))
-                 p12.start()                 
+                    p12 = Process(target=oscommandF, args=(url,))
+                    p12.start()                 
                  
-                 p13 = Process(target=ssrfF, args=(url,))
-                 p13.start()                 
+                    p13 = Process(target=ssrfF, args=(url,))
+                    p13.start()                 
                  
-                 p13.join(timeout=5)
-                 p12.join(timeout=5)
-                 p11.join(timeout=5)
-                 p10.join(timeout=10)
-                 p9.join(timeout=5)
-                 p6.join(timeout=5)                
-                 #p7.join(timeout=5)                 
-                 p8.join(timeout=5)
-            else:
-                pass
-        else:
-            for dlink in nano.inject_dir(i):
-                if dlink not in uniqlink :
-                    uniqlink.append(dlink)
-                    url=dlink
-                    px = Process(target=dirvulscanF, args=(url,))
-                    px.start()                    
-                    px.join(timeout=10)
+                    p13.join(timeout=5)
+                    p12.join(timeout=5)
+                    p11.join(timeout=5)
+                    p10.join(timeout=10)
+                    p9.join(timeout=5)
+                    p6.join(timeout=5)                
+                    #p7.join(timeout=5)                 
+                    p8.join(timeout=5)
                 else:
                     pass
+            else:
+                for dlink in nano.inject_dir(i):
+                    if dlink not in uniqlink :
+                        uniqlink.append(dlink)
+                        url=dlink
+                        px = Process(target=dirvulscanF, args=(url,))
+                        px.start()                    
+                        px.join(timeout=10)
+                    else:
+                        pass
         
         
         
