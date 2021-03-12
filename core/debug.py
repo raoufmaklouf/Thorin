236f39862d0dedcaac68b75c4c277457e51faeca
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-03-12 14:46:30 +0000

    Add files via upload

---
+++ core/debug.py
@@ -0,0 +1,43 @@
+import requests
+import random
+import sys
+import threading
+import queue
+
+pay=[
+'//wp-content/debug.log',
+'/assets/npm-debug.log',
+'/content/debug.log',
+'/data/debug/',
+'/debug',
+'/debug_error.jsp',
+'/debug-output.txt',
+'/debug.inc',
+'/debug.log',
+'/debug.php',
+'/debug.py',
+'/debug.txt',
+'/debug.xml',
+'/debug/',
+'/mysql_debug.sql',
+'/npm-debug.log',
+'/wp-content/debug.log
+]
+
+def debug_(i):
+    testlink=i+'/uniq_Str.log'
+    test_r = requests.get(testlink,verify=False)
+    test_scode=test_r.status_code
+         
+    for x in pay:
+        url=i+x
+        r = requests.get(url,verify=False)
+        scode=r.status_code
+            if str(scode)[0] == '2' or str(scode)[0] == '3' :
+                if str(scode) != str(test_scode):
+                    print("\033[94m[+] Possibly debug disclosure :\033[00m  "+brute)
+          
+
+
+
+
