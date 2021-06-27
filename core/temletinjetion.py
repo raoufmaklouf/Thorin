86439549b7aba62c7078538c7320bc752d4270e7
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-06-27 12:58:39 +0000

    Update temletinjetion.py

--- core/temletinjetion.py
+++ core/temletinjetion.py
@@ -9,22 +9,25 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 test1="58784"
 test2='32110'
-
+mt='InKgg67\[2s4h7jT67HF5)o[>,j'
 def ssti_(url):
    
     try:
-        for x in regex.payload_ssti_1:
-            r = requests.get(nano.inject_param(url,x),verify=False)
-            cont1 = r.content
-            if(re.search(test1, str(cont1))):
-                for i in regex.payload_ssti_2:
-                    r = requests.get(nano.inject_param(url,i),verify=False,timeout=10)
-                    cont2 = r.content
-                    if(re.search(test2, str(cont2))):
-                         print("\033[91m Possibly SS template injection vulnerability\033[00m\t"+nano.inject_param(url,i))
-                         break
-                    else:
-                         pass
+        for link in nano.injecter(url,mt):
+            for x in regex.payload_ssti_1:
+                url1=link.replace(mt,x)
+                r = requests.get(url1,verify=False,timeout=13)
+                cont1 = r.content
+                if(re.search(test1, str(cont1))):
+                    for i in regex.payload_ssti_2:
+                        url2=url1=link.replace(mt,i)
+                        r = requests.get(url2,verify=False,timeout=13)
+                        cont2 = r.content
+                        if(re.search(test2, str(cont2))):
+                            print("\033[91m Possibly SS template injection vulnerability\033[00m\t"+url2)
+                            break
+                        else:
+                            pass
     except:
         pass
         
