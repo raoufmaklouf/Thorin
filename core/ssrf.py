8ede55b88de79ca39d671d3e1526d345e211d044
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-06-27 12:57:46 +0000

    Update ssrf.py

--- core/ssrf.py
+++ core/ssrf.py
@@ -12,16 +12,17 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def ssrf_(i):
     
     try:
-        url=nano.inject_param(i,config.ssrf_link)
-        user_agent=random.choice(regex.USR_AGENTS)
-        headers = {'User-Agent': user_agent } 
-        r = requests.get(url ,headers=headers ,verify=False )       
-        resp = r.content
-        x = re.findall(config.ssrf_regix, str(resp))
-        if (x):
-            print('\033[91mPossibly SSRF vulnerability\033[00m  '+url)
-            msg="Possibly SSRF vulnerability "+url
-            bot.telegram_bot_sendtext(msg)
+        for url in nano.injecter(i,config.ssrf_link):
+      
+            user_agent=random.choice(regex.USR_AGENTS)
+            headers = {'User-Agent': user_agent } 
+            r = requests.get(url ,headers=headers ,verify=False,timeout=13 )       
+            resp = r.content
+            x = re.findall(config.ssrf_regix, str(resp))
+            if (x):
+                print('\033[91mPossibly SSRF vulnerability\033[00m  '+url)
+                msg="Possibly SSRF vulnerability "+url
+                bot.telegram_bot_sendtext(msg)
     except:
         pass
                        
