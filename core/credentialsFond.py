1db0e623ce312dcbc61e55c4fc48830884a8177f
raoufmaklouf <raoufmaklouf0int@gmail.com>
2021-06-29 11:45:58 +0000

    Update credentialsFond.py

--- core/credentialsFond.py
+++ core/credentialsFond.py
@@ -2,6 +2,7 @@ import re
 import random
 import requests
 from core import regex
+from core import counter
 from requests.packages import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -14,16 +15,17 @@ def CredentialsFond_(url):
     user_agent=random.choice(regex.USR_AGENTS)
     headers = {'User-Agent': user_agent } 
     try:
-        r = requests.get(url,headers=headers,verify=False)
+        r = requests.get(url,headers=headers,verify=False,timeout=13)
         resp = r.content
         if len(resp)>0:
            for a,b in regex.CredentialsFond_REGEX.items():
                mutch=re.search(b, str(resp))
                if(mutch):               
                    mt=mutch.group()
-                
-                   print("\033[94m[+] Possibly Sensitive data disclosure on: {} \033[00m".format(url))
-                   print("\033[33m[{}]-----|\033[00m  {}".format(a.rstrip(),mt))
+                   if mt not in counter.credentialsFond:
+                       counter.credentialsFond.append(mt)
+                       print("\033[94m[+] Possibly Sensitive data disclosure on: {} \033[00m".format(url))
+                       print("\033[33m[{}]-----|\033[00m  {}".format(a.rstrip(),mt))
                                 
         else:
             pass
@@ -33,4 +35,5 @@ def CredentialsFond_(url):
                             
                                 
             
+
 
